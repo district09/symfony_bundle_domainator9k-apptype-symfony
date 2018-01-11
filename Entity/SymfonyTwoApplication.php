f80f76e88dcc76447e195f574107a08dd7223725
Pieter MAssoels <massoelspieter@gmail.com>
2018-01-11 13:01:47 +0100

    WEBDOM-271 : file provisioning

--- Entity/SymfonyTwoApplication.php
+++ Entity/SymfonyTwoApplication.php
@@ -3,6 +3,7 @@
 namespace DigipolisGent\Domainator9k\AppTypes\SymfonyTwoBundle\Entity;
 
 use DigipolisGent\Domainator9k\CoreBundle\Entity\AbstractApplication;
+use DigipolisGent\Domainator9k\CoreBundle\Entity\TemplateInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -68,4 +69,18 @@ class SymfonyTwoApplication extends AbstractApplication
     {
         $this->secret = $secret;
     }
+
+    /**
+     * @return array
+     */
+    public static function getTemplateReplacements(): array
+    {
+        $templateReplacements = parent::getTemplateReplacements();
+        $templateReplacements['locale()'] = 'getLocale()';
+        $templateReplacements['secret()'] = 'getSecret()';
+
+        return $templateReplacements;
+    }
+
+
 }
