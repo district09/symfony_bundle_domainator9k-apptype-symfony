566e38560abc1cfa0cdb17d56294dc961ad74c2f
Pieter MAssoels <massoelspieter@gmail.com>
2018-02-12 12:13:59 +0100

    WEBDOM-281 : application implementation update

--- Entity/SymfonyTwoApplication.php
+++ Entity/SymfonyTwoApplication.php
@@ -2,6 +2,7 @@
 
 namespace DigipolisGent\Domainator9k\AppTypes\SymfonyTwoBundle\Entity;
 
+use DigipolisGent\Domainator9k\AppTypes\SymfonyTwoBundle\Form\Type\SymfonyTwoApplicationFormType;
 use DigipolisGent\Domainator9k\CoreBundle\Entity\AbstractApplication;
 use DigipolisGent\Domainator9k\CoreBundle\Entity\TemplateInterface;
 use Doctrine\ORM\Mapping as ORM;
@@ -18,9 +19,20 @@ class SymfonyTwoApplication extends AbstractApplication
 
     const TYPE = "SYMFONY_TWO";
 
-    public static function getType()
+    /**
+     * @return string
+     */
+    public static function getApplicationType() :string
     {
         return self::TYPE;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getFormType(): string
+    {
+        return SymfonyTwoApplicationFormType::class;
     }
 
     /**
@@ -81,6 +93,4 @@ class SymfonyTwoApplication extends AbstractApplication
 
         return $templateReplacements;
     }
-
-
 }
