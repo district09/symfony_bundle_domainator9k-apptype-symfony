5727c676ebc1b80bbdba0abdce85a9122d358d32
Pieter MAssoels <massoelspieter@gmail.com>
2018-04-27 15:24:41 +0200

    table rename

--- Entity/SymfonyTwoApplication.php
+++ Entity/SymfonyTwoApplication.php
@@ -13,6 +13,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @package DigipolisGent\Domainator9k\AppTypes\SymfonyTwoBundle\Entity
  *
  * @ORM\Entity()
+ * @ORM\Table(name="symfony_two_application")
  */
 class SymfonyTwoApplication extends AbstractApplication
 {
