6ab2d39aef3662537c4f1b00f523555183053c58
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-05 16:32:01 +0100

    Update to new directory structure for bundles

--- DigipolisGentDomainator9kAppTypesSymfonyTwoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DigipolisGent\Domainator9k\AppTypes\SymfonyTwoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DigipolisGentDomainator9kAppTypesSymfonyTwoBundle extends Bundle
-{
-}
